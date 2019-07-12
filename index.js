466aa84b13bed8ad81ab47256387a5df467385cc
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-12 23:52:17 +0100

    Add basic routing functionality to read pathnames and id query strings

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const fs = require('fs');
+const fs = require('fs'); //file system on this machine
 const http = require('http');
+const url = require('url');
 
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); // go find files locally and read them
-const laptopData = JSON.parse(json); // convert JSON to JavaScript object
+const laptopData = JSON.parse(json); // convert JSON to JavaScript object => array of 5 objects
 
-
+// HTTP request & HTTP response
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server!');
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    console.log(url.parse(req.url, true));
+
+    if (pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html' }) // header response w/ 200 meaning all is OK, with content text/html,
+        res.end('This is the PRODUCTS page'); // then response following the header. Can change 200 to 404 it'll go red :)
+    }
+
+    else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        res.end(`This is the LAPTOP page for laptop ${id}!`);
+    }
+
+    else {
+        res.writeHead(404, { 'Content-type': 'text/html' })
+        res.end('URL was not found on the server!');
+    }
+
 });
 
 // Port & server, with callback string to ensure it works
