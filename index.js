5271370dff9cd2dde935d828bccac552a86bb65e
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-07-12 23:16:03 +0100

    Add server response and http request variables to check if port and server works

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const fs = require('fs');
+const http = require('http');
+
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8'); // go find files locally and read them
+const laptopData = JSON.parse(json); // convert JSON to JavaScript object
+
+
+const server = http.createServer((req, res) => {
+    console.log('Someone did access the server!');
+});
+
+// Port & server, with callback string to ensure it works
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests now :)');
+});
